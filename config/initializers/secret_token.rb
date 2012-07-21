a8e107994f6b77ff0b243e8e56185d80d7be2cf2
Tad Thorley <phaedryx@gmail.com>
2012-07-20 21:31:02 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrugSsoProvider::Application.config.secret_token = '42de1aac04fb7f8ff876410902edfb5df95d4f61734bb6a29752681b7ce1a92585022ee57d6d37280bbfee9d94f36ad41cf059f9be08a8d4d5b86ab7bdd6cb26'
